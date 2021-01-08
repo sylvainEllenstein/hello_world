24663e0b81b1755e1c08e04afe362007e664178a
sylvainEllenstein <76899255+sylvainEllenstein@users.noreply.github.com>
2021-01-08 22:30:44 +0100

    Update prologin_lab-demoniaque.py
    Fonction meilleurChemin(p), nouveauChemin(i, c) terminées
    Variables fixées, autres fonctions déclarées, à compléter

--- prologin_lab-demoniaque.py
+++ prologin_lab-demoniaque.py
@@ -18,42 +18,38 @@
     n = int(input()) #longueur
     m = int(input()) #largeur
     plateau = [list(map(int, input().split(" "))) for _ in range(n)]
-    tous_chemins = [] for _ in range (0, m)
+	
+	
+    liste_chemins = [_] for _ in range (0, m) #décrit les chemins étudiés : list(list(int)) -> abscisses des cases des chemins
+	liste_poids = [plateau[0][i] for i in plateau[0]] #poid qui correspondent aux chemins dans liste_chemins
     
+	def poids(x_array):
+		#retourne le poids d'un array
+		#type(out) : int
+		pass
+	
     def cases_suivante(x, largeur):
         #retourne les cases suivantes d'un objet chemin 
         if x == 0: return [0, 1]
         elif x == largeur - 1: return [largeur - 1, largeur]
         return [x - 1, x, x + 1]
     
-    def meilleurChemin(tous_chemins):
+    def meilleurChemin(poids):
         #donne le(s) meilleur(s) chemin(s) actuel(s) dans tous les objets chemin
-        #entrée : tous_chemins, qui contient tous les objets chemin
-        #sortie : [place des chemins dans tou_chemins]
-        pass
+        #type(out) : list(int)
+        return [i for i,x in enumerate(poids) if x == min(poids)]
     
-    class chemin:
-        def __init__(self, c):
-            self.cases = c
-            self.poids = sum(c)
-            
-        def copier(self, nouvelle_case):
-        #copie le chemin self et créé un nouveau avec le nouvelle_case en plus
-            ch_ = chemin(self.cases)
-            pass
-        def meilleureCase(liste):
-        #calcule la prochaine meilleure case d'un objet chemin
-        #A améliorer : doit pouvoir prendre en compte si deux cases ont le même poids -> création d'un nouveau chemin
-        pass
-        
-        def nouvelleCase(self, case):
+	def meilleureCase(x_array):
+    	#calcule la prochaine meilleure case d'un objet chemin
+        #type(out) : list(int) -> x
+		pass
     
-    def main(chemins_possibles, plateau):
-        #création de tous les premiers chemins :
-        for i in range(0, m):
-            tous_chemins[i] = chemin(i)
-        
-        
+	def nouveauChemin(chemin_init, case):
+		chemin_init.append(case)
+		liste_chemins.append(chemin_init)
+		liste_poids.append(poids(chemin_init))
+	
+    def main(chemins_possibles, plateau):        
         #TODO : boucle qui prend le plus court objet chemin, le continue et actualise sa longueur
         #la boucle coupe si le plus court chemin est plus grand que l'âme du visiteur, et coupe les chemins déjà trop grands
         
