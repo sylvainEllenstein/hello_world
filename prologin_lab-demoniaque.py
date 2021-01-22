a4e8f378e4127d61a0a18ac0e0c74ab900aa805b
sylvainEllenstein <76899255+sylvainEllenstein@users.noreply.github.com>
2021-01-22 22:50:06 +0100

    Update prologin_lab-demoniaque.py
    Main() à faire et terminer, puis tester

--- prologin_lab-demoniaque.py
+++ prologin_lab-demoniaque.py
@@ -22,67 +22,88 @@
     liste_chemins = [[_] for _ in range (0, m)] #décrit les chemins étudiés : list(list(int)) -> abscisses des cases des chemins
 	liste_poids = [plateau[0][i] for i in plateau[0]] #poid qui correspondent aux chemins dans liste_chemins
     
-	def poids(x_array, plateau):
-		#retourne le poids d'un array
+	def poids(x_array):
+		#retourne le poids d'un x_array
 		#type(out) : int
-		return sum([plateau[i][x_array[i]] for i in range(len(x_array))]
+		global plateau
+		return sum([plateau[i][x_array[i]] for i in range(len(x_array))])
 				   
-    def cases_suivantes(x, largeur):
-        #retourne les cases suivantes d'un chemin 
+    def cases_suivantes(x):
+        #retourne les cases suivantes possibles à partir de l'abscisse d'une case, sans trier avec la meilleure
+
+        global m
         if x == 0: return [0, 1]
-    	elif x == largeur - 1: return [largeur - 1, largeur]
+    	elif x == m - 1: return [m - 1, m]
         return [x - 1, x, x + 1]
-    
+
+
     def meilleurChemin(poids):
         #donne le(s) meilleur(s) chemin(s) actuel(s) dans tous les chemins
         #type(out) : list(int) -> place des chemins dans liste_chemins
         return [i for i,x in enumerate(poids) if x == min(poids)]
-    
-	def meilleureCase(x_array,plateau, m):
-    	#calcule la prochaine meilleure case d'un objet chemin
-        #type(out) : list(int) -> x
-		suivantes = cases_suivantes(x_array[::-1][0], m)
-		valeur = lambda x : plateau[len(x_array)][x]
-		meilleures = [i for i in suivantes if valeur[i] == min(list(valeur(x) for x in suivantes))]
+
+	def meilleureCase(x_array):
+    	#calcule les prochaines meilleures cases d'un chemin
+        #type(out) : list(int) -> les 'x' correspondant à la case en question
+		suivantes = cases_suivantes(x_array[::-1][0]) # = les cases suivantes du chemin donné par x_array
+		valeurs = [plateau[len(x_array)][i for i in suivantes]] #les valeurs des cases correspondant aux suivantes
+		return [suivantes[i] for i in range (0, len(suivantes) if valeurs[i] == min(valeurs)]
+
+
 
 	def copieChemin(chemin_init, case):
-		#copie un chemin existant en rajoutant la case 'case'
+		#copie un chemin existant (x_array) en rajoutant la case 'case'
+		#actualise les poids
 		chemin_init.append(case)
 		global liste_chemins
 		liste_chemins.append(chemin_init)
 		liste_poids.append(poids(chemin_init))
 	
 	def nouveauxChemins(x_array):
-		#entrée : le chemin à continuer (choisi par meilleurChemin dans le main)
+		#entrée : un chemin (x_array)
 		# -> continue / créé de nouveaux x_arrays (avec copieChemin) si nécessaire
 		# -> coupe les chemins créés trop grands
+		#OUT : tous les chemins restants 
+		#type(out) : -> void (???)
 		global a #ame du visiteur
+		if meilleureCase(x_array)[0] + poids(x_array) <= a:
+			copieChemin(x_array, meilleureCase(x_array)[0])
+
+		#boucle qui s'éxécute si il y a plusieurs cases suivantes possibles
+		for i in meilleureCase(x_array)[1:]:
+			if poids(x_array) + plateau[len(x_array)][i] <= a:
+				copieChemin(x_array, i)
+
+
 		
+##################  MAIN  ##################################
+
+    #TODO : boucle qui prend le plus court chemin
+    # -> le supprime avec .pop() de liste_chemins
+    # -> supprimer aussi le numéro de liste_poids correcpondant
+    # -> envoyé à nouveauxChemins
+    # -> .extend() sur liste_chemins des nouveauxChemins() du x_array
+    # ...
 				   
-    def main():      
-        #TODO : boucle qui prend le plus court objet chemin, le continue et actualise sa longueur
-        #la boucle coupe si le plus court chemin est plus grand que l'âme du visiteur, et coupe les chemins créés déjà trop grands
-		global liste_chemins
-		global a
-		global m
-		global n
-		global plateau
-		global liste_poids
-				   
-				   
-        mainloop = ((max[(len(i)) for i in liste_chemins] < n) or ( #si le chemin qui a atteint le bord n'est pas le plus court
-		)) and (meilleurChemin(liste_poids) <= a) 
-		#tant que le plus court chemin n'est pas trop grand ------> si liste_chemins == []
-		#aucun chemin chemin n'atteint le bord ou si le chemin qui a atteint le bord n'est pas le plus court
+    mainloop = True 
+ ##################  CONDITIONS MAINLOOP  ##################
+
+    #si le chemin qui a atteint le bord n'est pas le plus court 
+	#tant que le plus court chemin n'est pas trop grand ------> si liste_chemins == []
+	#aucun chemin chemin n'atteint le bord ou si le chemin qui a atteint le bord n'est pas le plus court
+
+ ###########################################################
+
+	while mainloop:
+		#continue un chemin SI : plus court (ET non-fini)
+		# + actualiser les poids : se fait lors dela crétion/poursuite des chemins
+		# + couper les chemins si déjà trop grands (créer les chemins un par un)
+		# + deplacer les chemins validés dans une autre liste
+
+		pass
+		mainloop = False
 		
-		while mainloop:
-			#continue un chemin SI : plus court ET non-fini
-			# + actualiser les pooids
-			# + couper les chemins si déjà trop grands (créer les chemins un par un)
-			pass
-			mainloop = False
-		
-	main()
+	
     
         
 labyrinthe_demoniaque()
